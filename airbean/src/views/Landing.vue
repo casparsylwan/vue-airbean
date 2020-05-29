b76a220b8e0b6ef3969eae337a08edf1665263de
casparsylwan <casparsylwan@gmail.com>
2020-05-29 11:06:35 +0200

    landing page done with grid

--- airbean/src/views/Landing.vue
+++ airbean/src/views/Landing.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="landing">
     
-    <imgHeader class="img-left"></imgHeader>
+    <FlowerLeft class="left"></FlowerLeft>
+    
     <landingImage class="img-center"></landingImage>
     
-    <imgBottom class="img-right"></imgBottom>
+    <FlowerRight class="right"></FlowerRight>
 
     
   </div>
@@ -12,18 +13,21 @@
 <script>
 
 import landingImage from '../components/svg/airbean-landing';
-import imgHeader from '../components/svg/graphics-header';
-import imgBottom from '../components/svg/graphics-footer';
+import FlowerLeft from '../components/svg/flowerleft';
+import FlowerRight from '../components/svg/FlowerRight';
+
+
 export default {
   name: 'Landing',
   components: {
     landingImage,
-    imgHeader,
-    imgBottom,
+    FlowerLeft,
+    FlowerRight
+    
   },
 
   created(){
-      setTimeout( () => this.$router.push({ path: '/home'}), 3000);
+    //  setTimeout( () => this.$router.push({ path: '/home'}), 3000);
 
     }
   }
@@ -32,37 +36,62 @@ export default {
 <style scoped lang="scss">
 
 .landing {
-    min-height: 100vh;
-    background-color: rgb(35, 145, 103);
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-
-   
     
-}
-
-.img-left{
-  position: relative;
-  margin-left: -50vh;
-   min-width: 100vh;
-   
-  transform: rotate(270deg);
-}
-.img-right{
-  position: relative; 
-  min-width: 100vh;
- margin-left: -60vh;
-  
-  transform:  rotate(-90deg);
+    background-color: rgb(35, 145, 103);
+    
+    display: grid;
+    grid-template-columns: 2fr 1fr 2fr;
+    grid-template-areas: 
+    "left . right"
+    "left center right"
+    "left . right";
+    
+         
 }
 
 .img-center{
-  position: absolute;
-  top: 40vh;
-  left: 7vh;
-
+  grid-area: center;
+  justify-self: center;
+  align-self: center;
+  padding-left: 3rem;
+ 
 }
+
+
+.left{
+ grid-area: left;
+ justify-self: start;
+ 
+}
+
+.right{
+ grid-area: right;
+ justify-self: end;
+ 
+ 
+}
+
+
+// .img-left{
+//   position: relative;
+//   margin-left: -50vh;
+//    min-width: 100vh;
+   
+//   transform: rotate(270deg);
+// }
+// .img-right{
+//   position: relative; 
+//   min-width: 100vh;
+//  margin-left: -60vh;
+  
+//   transform:  rotate(-90deg);
+// }
+
+// .img-center{
+//   position: absolute;
+//   top: 40vh;
+//   left: 7vh;
+
+// }
 
 </style>
